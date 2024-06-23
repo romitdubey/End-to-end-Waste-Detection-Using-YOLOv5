ba0c08299d5cc0bc00265d5d023920a2174ce127
Romit Dubey <romitdubey1@gmail.com>
2024-06-23 16:58:35 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='wasteDetection',
+    version="0.0.0",
+    author="Romit Dubey",
+    author_email="romitdubey1@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
